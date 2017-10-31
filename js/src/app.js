40d89e6b9904f3b0186402aa3d6843e723cace1f
ryanbmarx <ryanbmarx@gmail.com>
2017-10-31 14:08:17 -0500

    use keypress so arrow keys don't trigger a reflow of the autocomplete

--- js/src/app.js
+++ js/src/app.js
@@ -1,97 +1,7 @@
 import 'awesomplete';
+import translateData from './transform-data.js';
+import formatSchoolProfile from './format-profile.js';
 import {scaleLinear} from 'd3-scale';
-
-function translateData(data){
-	// A crosswalk, of sorts, to take the data from the database and make it more usable per the design I like.
-	let retval = {
-	    name:data.name,
-	    district: `${data.district} (${data.city}, ${data.county} County)`,
-	    specialEd: parseFloat(data.iep_schl),
-	    freeLunch: parseFloat(data.low_income_schl),
-	    englishLearner: parseFloat(data.lep_schl),
-	    nonWhite: 100 - parseFloat(data.schl_white)
-	}
-
-	if(data.gr11_schl_avg_scale_score_in_ela_sat_2017_ela != "" && data.gr11_schl_avg_scale_score_in_math_sat_2017_math != "" && data.gr11_schl_ttl_scale_score_sat_2017_composite != ""){
-		retval.sat = {
-	        overall: {
-	            min: 400,
-	            max: 1600,
-	            median: 1000,
-	            school: parseFloat(data.gr11_schl_ttl_scale_score_sat_2017_composite)
-	        },
-	        math: {
-	            min: 200,
-	            max: 800,
-	            median: 500,
-	            school: parseFloat(data.gr11_schl_avg_scale_score_in_math_sat_2017_math)
-	        },
-	        ela: {
-	            min: 200,
-	            max: 800,
-	            median: 500,
-	            school: parseFloat(data.gr11_schl_avg_scale_score_in_ela_sat_2017_ela)
-	        }
-	    }
-
-	} else {
-		retval.sat = false;
-	}
-
-	// -----------
-	// PARCC SCORES
-	// -----------
-	// Test if the school has an overall parcc composite. if so, initialize our data containers.
-	retval.parcc = data.schl_pct_of_prof_ela_math_parcc_2017_composite != "" ? {grades:{}, overall:{}} : false;
-	
-	let keys = [
-		{letters: "third", num: "3"},
-		{letters: "fourth", num: "4"},
-		{letters: "fifth", num: "5"},
-		{letters: "sixth", num: "6"},
-		{letters: "seventh", num: "7"},
-		{letters: "eighth", num: "8"}
-	];
-
-	if (retval.parcc){
-		// Load up the school-level proficency
-		retval.parcc.overall = {
-			math:parseFloat(data.schl_pct_of_prof_in_math_parcc_2017_math),
-			ela:parseFloat(data.schl_pct_of_prof_in_ela_parcc_2017_ela),
-			composite:parseFloat(data.schl_pct_of_prof_ela_math_parcc_2017_composite)
-		};
-
-		keys.forEach(key => {
-			const 	letters = key.letters,
-					num = key.num;
-
-			if (data[`gr${num}_ela_schl_approached_expectns_parcc_all`] || data[`gr${num}_ela_schl_did_not_meet_expectns_parcc_all`] || data[`gr${num}_ela_schl_exceeded_expectns_parcc_all`] || data[`gr${num}_ela_schl_met_expectns_parcc_all`] || data[`gr${num}_ela_schl_partially_met_expectns_parcc_all`]){
-				retval['parcc']['grades'][letters] = {
-					 math:{
-		                DNM: parseFloat(data[`gr${num}_math_schl_did_not_meet_expectns_parcc_all`]),
-		                PM: parseFloat(data[`gr${num}_math_schl_partially_met_expectns_parcc_all`]),
-		                A: parseFloat(data[`gr${num}_math_schl_approached_expectns_parcc_all`]),
-		                M: parseFloat(data[`gr${num}_math_schl_met_expectns_parcc_all`]),
-		                E: parseFloat(data[`gr${num}_math_schl_exceeded_expectns_parcc_all`])
-		            }, 
-		            ela:{
-		                DNM: parseFloat(data[`gr${num}_ela_schl_did_not_meet_expectns_parcc_all`]),
-		                PM: parseFloat(data[`gr${num}_ela_schl_partially_met_expectns_parcc_all`]),
-		                A: parseFloat(data[`gr${num}_ela_schl_approached_expectns_parcc_all`]),
-		                M: parseFloat(data[`gr${num}_ela_schl_met_expectns_parcc_all`]),
-		                E: parseFloat(data[`gr${num}_ela_schl_exceeded_expectns_parcc_all`])
-		            }
-				}
-			} else {
-				retval['parcc']['grades'][letters] = false;
-			}
-		})
-
-	}
-	console.log(data, retval);
-		return retval;
-	}
-
 
 window.addEventListener('DOMContentLoaded', function(e){
 	
@@ -105,10 +15,10 @@ window.addEventListener('DOMContentLoaded', function(e){
 		const auto = new Awesomplete(searchBar, {
 			minChars: 2,
 			maxItems: 20,
-			autoFirst: true
+			// autoFirst: true
 		});
 
-		searchBar.addEventListener('keyup', function(e){
+		searchBar.addEventListener('keypress', function(e){
 			// When the user starts typing, it will ping the database to fetch a JSON list of possible schools
 			// There are three GET vars in the url:
 			// @format ... duh. we want JSON
@@ -132,8 +42,6 @@ window.addEventListener('DOMContentLoaded', function(e){
 
 				if (schoolsList.length > 0){
 					auto.list = schoolsList.map(i => [`${i.autocomplete} (${i.district})`, i.school_id]);
-				} else {
-					auto.list = "No matches";
 				}
 			}
 			ajax.send();
@@ -196,168 +104,3 @@ window.addEventListener('DOMContentLoaded', function(e){
 	})
 
 })
-
-function addPie(num, addClass=""){
-	return `<div class='pie pie--${Math.round(num)} ${addClass}'></div><span>${Math.round(num)}%</span>`;
-}
-
-function formatSchoolProfile(data){
-	// The data fetching mechanism is TBD. 
-
-	// Fill out the school name/district
-	document.querySelector('.school__name').innerHTML = data.name;
-	document.querySelector('.school__district').innerHTML = data.district;
-	
-	// Fill out the demographic big numbers
-	document.querySelector('.demo--lunch dt').innerHTML = addPie(data.freeLunch);
-	document.querySelector('.demo--ell dt').innerHTML = addPie(data.englishLearner);
-	document.querySelector('.demo--non-white dt').innerHTML = addPie(data.nonWhite);
-	document.querySelector('.demo--special-ed dt').innerHTML = addPie(data.specialEd);
-	
-	let satString = "", // This string will hold the html as it's written for SAT scores
-		parccSchoolProficencyString = "",
-		parccString = ""; // This string will hold the html as it's written for PARCC scores
-
-	if(data.sat){
-	
-		// Now, move on to the score charts
-		// This array of the keys will let us iterate over the tests
-		const tests = Object.keys(data.sat);
-
-		tests.forEach(test => {
-			// For each test, grab/calc the various stats needed
-
-			const 	temp = data['sat'][test],
-					min = temp.min,
-					max = temp.max,
-					median = temp.median,
-					school = temp.school,
-					satScale = scaleLinear().domain([min, max]).range([0, 100]),
-					medianPlacement = median ? satScale(median) : null,
-					schoolPlacement = satScale(school);
-			let label = window.testLabels[test];
-
-			// Add the element to the holder string
-			satString += `
-				<li class='score'>
-					<span class='score__label'>${label}</span>
-					<div class='score__chart'>`;
-
-			if (min > -1) {
-				satString += `						
-					<div class='test test--min' style='left: 0'>
-						<span class='test__dot'></span>
-						<span class='test__score'>${min}</span>
-					</div>`;
-			}	
-			if (median) {
-				satString += `<div class='test test--med' style='left: ${medianPlacement}%'>
-					<span class='test__dot'></span>
-					<span class='test__score'>${median}</span>
-				</div>`;
-			}
-
-				satString += `<div class='test test--max' style='left: 100%'>
-					<span class='test__dot'></span>
-					<span class='test__score'>${max}</span>
-				</div>`;
-
-				satString += `<div class='test test--school' style='left: ${schoolPlacement}%'>
-						<span class='test__dot'></span>
-						<span class='test__score'>${school}</span>
-					</div>`;
-			
-			satString += `</div></li>`;
-		});
-	
-	} else{
-		satString = `<p class='note'>${window.noScores}</p>`;
-	}
-
-	if (data.parcc){
-
-		// ---------- 
-		// START WITH THE OVERALL STUFF
-		// ----------
-		const parccOverall = data.parcc.overall;
-
-		parccSchoolProficencyString = `<h4 class='school__scores-sublabel'>${window.overallParccLabel}<span>${window.overallParccSubLabel}</span></h4>`;
-		parccSchoolProficencyString += `<ul class='school__scores school__scores--parcc'>`
-		
-		Object.keys(parccOverall).forEach(level => {
-			parccSchoolProficencyString += `<li class='score'><span class='score__label'>${level}</span><div class='score__chart score__chart--parcc'>`;
-			parccSchoolProficencyString += `<div class='parcc parcc--pm' style='width:${100 - parccOverall[level]}%'><span>${100 - parccOverall[level]}%</span></div>`;								
-			parccSchoolProficencyString += `<div class='parcc parcc--m' style='width:${parccOverall[level]}%'><span>${parccOverall[level]}%</span></div>`;
-
-		})
-
-		console.log(parccSchoolProficencyString);
-		// ---------- 
-		// NOW WITH THE GRADE LEVEL STUFF
-		// ----------
-
-		const 	testTypes = ['ela', 'math'],
-				parccLevels = Object.keys(data.parcc.grades);
-
-		parccLevels.forEach(level => {
-			if (level){
-				// Format the level string (i.e. "third" => "Third grade");
-				let levelString = level == "school" ? "Overall" : `${level} grade`;
-
-				// Load the sub label into the string 
-				parccString += `<h4 class='school__scores-sublabel'>${levelString}</h4>`;	
-
-				if (data['parcc']['grades'][level]){
-					// If there are scores for this grade level, then display them
-					parccString += `<ul class='school__scores school__scores--parcc'>`;	
-	
-					testTypes.forEach(type => {
-						const tempScores = data['parcc']['grades'][level][type];
-
-						// A little fix for the floated items. Totals might exceed 100% due to rounding
-						if (tempScores.DNM + tempScores.PM + tempScores.A + tempScores.M + tempScores.E > 100) {
-							if (tempScores.A > 0){
-								tempScores.A -= 0.1;				
-							} else {
-								tempScores.M -= 0.1;								
-							}
-						}
-						parccString += `<li class='score'><span class='score__label'>${window.testLabels[type]}</span><div class='score__chart score__chart--parcc'>`;
-						
-						// Add each bar only if the value is > 0;		
-						["dnm", "pm", "a", "m", "e"].forEach(result =>{
-							if (tempScores[result.toUpperCase()] > 0){
-								parccString += `<div class='parcc parcc--${result}' style='width:${tempScores[result.toUpperCase()]}%'><span>${Math.round(tempScores[result.toUpperCase()] * 10, -1) / 10}%</span></div>`								
-							}
-						});
-	
-						parccString += `</div></li>`;	
-					});
-					parccString += `</ul>`;	
-				} else {
-					// If there are no scores for this grade level, then don't display them.
-					parccString += `<p class='note'>${window.noScores}</p>`;
-				}
-			}
-		})
-	} else{
-		parccString = `<p class='note'>${window.noScores}</p>`;
-	}
-
-	// Insert our parsed SAT chart string into the profile
-	document.querySelector('#school-scores').innerHTML = satString;
-
-	// Insert our parsed PARCC chart string into the profile
-	document.querySelector('#parcc-scores').innerHTML = parccString;
-	document.querySelector('#parcc-scores-overall').innerHTML = parccSchoolProficencyString;
-
-	// Display (unhide) the profile
-	document.querySelector('.school').classList.add('school--active');
-}
-
-
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
-}
